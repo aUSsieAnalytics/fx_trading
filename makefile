58ebdcb3eeef82fbf6a499e1d4b2f5010a371f16
Peter Somers <37300408+psomers3@users.noreply.github.com>
2025-02-06 14:37:20 +0100

    Add code coverage check (#2)
    * add variables to test actions
    
    * try updating clang-format
    
    * typo in runs-on
    
    * add sudo
    
    * forgot checkout
    
    * add sudo in correct spot
    
    * format and remove unknown format key
    
    * debugging
    
    * cleaning up
    
    * add space
    
    * add meta step just to see if it works
    
    * proper naming
    
    * yaml -> yml
    
    * missing .
    
    * fix path
    
    * specify shell
    
    * remove run level
    
    * remove default
    
    * trying to get coverage to work
    
    * got coverage working for mac
    
    * add coverage check
    
    * make folder first
    
    * install lcov
    
    * add sudo
    
    * adjust to work on mac and ubuntu
    
    * move more flags to mac only
    
    * add checks for start and end time

--- makefile
+++ makefile
@@ -1,8 +1,11 @@
 test:
-	bazel test //...
+	bazel test //... --test_output=errors --test_verbose_timeout_warnings
 
 format:
 	sh ./ci/format.sh
 
 format-check:
 	sh ./ci/check_formatting.sh
+
+coverage:
+	sh ./ci/coverage.sh
--- simulator/tests/test_simulator.cpp
+++ simulator/tests/test_simulator.cpp
@@ -8,4 +8,6 @@ TEST(SimulatorTest, Initialization) {
   auto simulator = FXSimulator();
   ASSERT_NEAR(simulator.current_time(),
               chrono::system_clock::to_time_t(chrono::system_clock::now()), 1e2);
+  ASSERT_EQ(simulator.start_time(), 0);
+  ASSERT_EQ(simulator.end_time(), 0);
 }
