fe47ee34540220cd6c749d6f43d809f93907fc73
psomers3 <peter.w.somers@icloud.com>
2025-02-06 10:26:44 +0100

    format and remove unknown format key

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -1,7 +1,7 @@
 #include "simulator.hpp"
 #include <chrono>
 
-FXSimulator::FXSimulator(): _start_time(0), _end_time(0) {
+FXSimulator::FXSimulator() : _start_time(0), _end_time(0) {
   _current_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
 }
 
