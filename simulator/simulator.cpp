ba7f006637f023c7a434b07e45f20e91fd055a73
psomers3 <peter.w.somers@icloud.com>
2025-02-07 13:10:49 +0100

    no general namespace usage

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -2,7 +2,7 @@
 #include <chrono>
 
 FXSimulator::FXSimulator() : _end_time(0), _start_time(0) {
-  _current_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+  _current_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 }
 
 time_t FXSimulator::current_time() { return _current_time; }
