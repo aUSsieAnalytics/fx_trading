7d4e9e095ae9e00e8dc55e568cbd1220b25d5687
psomers3 <peter.w.somers@icloud.com>
2025-02-10 13:04:37 +0100

    fix push node vector

--- data_node/DataNode.hpp
+++ data_node/DataNode.hpp
@@ -8,21 +8,21 @@ template <typename T> class DataNode;
 
 typedef std::variant<std::shared_ptr<DataNode<int>>, std::shared_ptr<DataNode<double>>,
                      std::shared_ptr<DataNode<std::string>>>
-    DataShrPtr;
+    DataNodeShrPtr;
 typedef std::variant<std::weak_ptr<DataNode<int>>, std::weak_ptr<DataNode<double>>,
                      std::weak_ptr<DataNode<std::string>>>
-    DataWeakPtr;
+    DataNodeWeakPtr;
 
 template <typename T> class DataNode {
   std::vector<T> _data;
-  std::vector<DataShrPtr> _pull_data;
-  std::vector<DataWeakPtr> _push_data;
+  std::vector<DataNodeShrPtr> _pull_data;
+  std::vector<DataNodeWeakPtr> _push_data;
 
 public:
-  DataNode() {}
-  std::vector<DataShrPtr> get_pull_data() { return _pull_data; }
-  std::vector<DataWeakPtr> get_push_data() { return _push_data; }
-  void add_pull_data(DataShrPtr data) { _pull_data.push_back(data); }
-  void add_push_data(DataShrPtr data) { _push_data.push_back(data); }
+  DataNode() : _data(), _pull_data(), _push_data() {}
+  std::vector<DataNodeShrPtr> get_pull_data() { return _pull_data; }
+  std::vector<DataNodeWeakPtr> get_push_data() { return _push_data; }
+  void add_pull_node(DataNodeShrPtr data) { _pull_data.push_back(data); }
+  void add_push_node(DataNodeWeakPtr data) { _push_data.push_back(data); }
   virtual std::vector<T> get_data() { return _data; };
 };
