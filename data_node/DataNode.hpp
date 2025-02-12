78b4917911a5c7c763b3d002f72cc7ad2a05f107
psomers3 <peter.w.somers@icloud.com>
2025-02-12 13:35:30 +0100

    move away from variant approach

--- data_node/DataNode.hpp
+++ data_node/DataNode.hpp
@@ -4,25 +4,29 @@
 #include <time.h>
 #include <vector>
 
-template <typename T> class DataNode;
+class IDataNode;
 
-typedef std::variant<std::shared_ptr<DataNode<int>>, std::shared_ptr<DataNode<double>>,
-                     std::shared_ptr<DataNode<std::string>>>
-    DataNodeShrPtr;
-typedef std::variant<std::weak_ptr<DataNode<int>>, std::weak_ptr<DataNode<double>>,
-                     std::weak_ptr<DataNode<std::string>>>
-    DataNodeWeakPtr;
+typedef std::shared_ptr<IDataNode> DataNodeShrPtr;
+typedef std::weak_ptr<IDataNode> DataNodeWeakPtr;
 
-template <typename T> class DataNode {
-  std::vector<T> _data;
-  std::vector<DataNodeShrPtr> _pull_nodes;
-  std::vector<DataNodeWeakPtr> _push_nodes;
+class IDataNode {
+  std::vector<DataNodeShrPtr> _upstream_nodes;
+  std::vector<DataNodeWeakPtr> _downstream_nodes;
+  friend std::shared_ptr<std::vector<DataNodeShrPtr>>
+  get_leaf_nodes(std::shared_ptr<IDataNode> node,
+                 std::shared_ptr<std::vector<DataNodeShrPtr>> leaf_nodes);
 
 public:
-  DataNode() : _data(), _pull_nodes(), _push_nodes() {}
-  std::vector<DataNodeShrPtr> get_pull_nodes() { return _pull_nodes; }
-  std::vector<DataNodeWeakPtr> get_push_nodes() { return _push_nodes; }
-  void add_pull_node(DataNodeShrPtr data) { _pull_nodes.push_back(data); }
-  void add_push_node(DataNodeWeakPtr data) { _push_nodes.push_back(data); }
+  IDataNode() : _upstream_nodes(), _downstream_nodes() {}
+  std::vector<DataNodeShrPtr> get_upstream_nodes() { return _upstream_nodes; }
+  std::vector<DataNodeWeakPtr> get_downstream_nodes() { return _downstream_nodes; }
+  void add_upstream_node(DataNodeShrPtr data) { _upstream_nodes.push_back(data); }
+  void add_downstream_node(DataNodeWeakPtr data) { _downstream_nodes.push_back(data); }
+};
+
+template <typename T> class DataNode : public IDataNode {
+  std::vector<T> _data;
+
+public:
   virtual std::vector<T> get_data() { return _data; };
 };
--- data_node/tests/test_datanode.cpp
+++ data_node/tests/test_datanode.cpp
@@ -7,12 +7,12 @@ using namespace std;
 TEST(DataTest, Initialization) { auto source = DataNode<int>(); }
 
 TEST(DataTest, AddSource) {
-  std::shared_ptr<DataNode<int>> root = std::make_shared<DataNode<int>>();
+  auto root = std::make_shared<DataNode<int>>();
   auto second = std::make_shared<DataNode<int>>();
-  ASSERT_EQ(root->get_pull_nodes().size(), 0);
-  root->add_pull_node(second);
-  ASSERT_EQ(root->get_pull_nodes().size(), 1);
-  ASSERT_EQ(root->get_push_nodes().size(), 0);
-  second->add_push_node(root);
-  ASSERT_EQ(second->get_push_nodes().size(), 1);
+  ASSERT_EQ(root->get_upstream_nodes().size(), 0);
+  root->add_upstream_node(second);
+  ASSERT_EQ(root->get_upstream_nodes().size(), 1);
+  ASSERT_EQ(root->get_downstream_nodes().size(), 0);
+  second->add_downstream_node(root);
+  ASSERT_EQ(second->get_downstream_nodes().size(), 1);
 }
