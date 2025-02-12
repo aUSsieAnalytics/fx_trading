2feca26c73b7fd7096c0c1b81b5268e0722d2cf6
psomers3 <peter.w.somers@icloud.com>
2025-02-12 14:58:44 +0100

    finish leaf node getter

--- data_node/DataNode.hpp
+++ data_node/DataNode.hpp
@@ -9,19 +9,35 @@ class IDataNode;
 typedef std::shared_ptr<IDataNode> DataNodeShrPtr;
 typedef std::weak_ptr<IDataNode> DataNodeWeakPtr;
 
-class IDataNode {
+class IDataNode : public std::enable_shared_from_this<IDataNode> {
   std::vector<DataNodeShrPtr> _upstream_nodes;
   std::vector<DataNodeWeakPtr> _downstream_nodes;
   friend std::shared_ptr<std::vector<DataNodeShrPtr>>
   get_leaf_nodes(std::shared_ptr<IDataNode> node,
-                 std::shared_ptr<std::vector<DataNodeShrPtr>> leaf_nodes);
+                 std::shared_ptr<std::vector<DataNodeShrPtr>> leaf_nodes = nullptr) {
+    {
+      if (leaf_nodes == nullptr) {
+        leaf_nodes = std::make_shared<std::vector<DataNodeShrPtr>>();
+      }
+      if (node->_upstream_nodes.size() == 0) {
+        leaf_nodes->push_back(node);
+      }
+      for (auto &upstream_node : node->_upstream_nodes) {
+        get_leaf_nodes(upstream_node, leaf_nodes);
+      }
+      return leaf_nodes;
+    }
+  }
 
 public:
+  DataNodeWeakPtr get_weak_ptr() { return shared_from_this(); };
   IDataNode() : _upstream_nodes(), _downstream_nodes() {}
   std::vector<DataNodeShrPtr> get_upstream_nodes() { return _upstream_nodes; }
   std::vector<DataNodeWeakPtr> get_downstream_nodes() { return _downstream_nodes; }
-  void add_upstream_node(DataNodeShrPtr data) { _upstream_nodes.push_back(data); }
-  void add_downstream_node(DataNodeWeakPtr data) { _downstream_nodes.push_back(data); }
+  void add_upstream_node(DataNodeShrPtr data) {
+    _upstream_nodes.push_back(data);
+    data->_downstream_nodes.push_back(get_weak_ptr());
+  }
 };
 
 template <typename T> class DataNode : public IDataNode {
--- data_node/tests/test_datanode.cpp
+++ data_node/tests/test_datanode.cpp
@@ -8,11 +8,20 @@ TEST(DataTest, Initialization) { auto source = DataNode<int>(); }
 
 TEST(DataTest, AddSource) {
   auto root = std::make_shared<DataNode<int>>();
-  auto second = std::make_shared<DataNode<int>>();
+  auto second = std::make_shared<DataNode<double>>();
   ASSERT_EQ(root->get_upstream_nodes().size(), 0);
   root->add_upstream_node(second);
   ASSERT_EQ(root->get_upstream_nodes().size(), 1);
   ASSERT_EQ(root->get_downstream_nodes().size(), 0);
-  second->add_downstream_node(root);
   ASSERT_EQ(second->get_downstream_nodes().size(), 1);
+}
+
+TEST(DataTest, CollectLeafNodes) {
+  auto root = std::make_shared<DataNode<int>>();
+  auto leaf1 = std::make_shared<DataNode<double>>();
+  auto second = std::make_shared<DataNode<int>>();
+  root->add_upstream_node(second);
+  auto leaves = get_leaf_nodes(root);
+  ASSERT_EQ(leaves->size(), 1);
+  ASSERT_EQ(get_leaf_nodes(leaf1)->size(), 1);
 }
