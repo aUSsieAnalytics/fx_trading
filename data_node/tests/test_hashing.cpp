b4ffebee2ed86aa1991a1671804efaed851a7c35
psomers3 <peter.w.somers@icloud.com>
2025-02-19 22:06:28 +0100

    make test better

--- data_node/tests/test_hashing.cpp
+++ data_node/tests/test_hashing.cpp
@@ -9,5 +9,6 @@ TEST(HashingTest, TestHashOutput) {
   ASSERT_EQ(output1, "0f78fcc486f5315418fbf095e71c0675ee07d318e5ac4d150050cd8e57966496");
 
   string huge_string((512 / 8) * 5, 'x');
-  sha256(huge_string);
+  ASSERT_EQ(sha256(huge_string),
+            "e894706a0b642bfb3eb07c97fee00b18ab1581ff661f1c4473f55e6afe1c8982");
 }
