ffba07674fd927c3a524ae5c3875b894adfa309f
psomers3 <peter.w.somers@icloud.com>
2025-02-21 13:05:29 +0100

    registry working and full example working

--- data_node/DataNode.hpp
+++ data_node/DataNode.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "sha256.hpp"
+#include "uuid.hpp"
+#include <functional>
 #include <memory>
 #include <mutex>
 #include <sstream>
@@ -19,6 +21,8 @@ class DataStore {
 public:
   DataStore() : _store() {};
 
+private:
+  template <typename ClassName, typename T> friend class DataNode;
   template <typename ClassName, typename T>
   void put_in_store(DataNode<ClassName, T> *node, std::shared_ptr<std::vector<T>> data) {
     _store[node->get_hash()] = data;
@@ -34,15 +38,19 @@ public:
 };
 
 class IDataNode : public std::enable_shared_from_this<IDataNode> {
+  static inline std::unordered_map<std::string, DataNodeShrPtr> __registry__;
   static inline std::mutex _scope_lock;
   static inline std::string _scope;
   std::string _class_scope;
   std::string _class_name;
   std::vector<DataNodeShrPtr> _upstream_nodes;
-  std::vector<DataNodeWeakPtr> _downstream_nodes;
+  std::vector<std::string> _downstream_node_hashes;
+  std::vector<DataNodeShrPtr> _output_nodes;
+  std::string _parent_hash;
+  std::function<void(std::shared_ptr<void> onode)> _parent_calc;
   std::string _hash;
   std::string _hash_string;
-
+  template <typename ClassName, typename T> friend class DataNode;
   friend std::ostream &operator<<(std::ostream &stream, IDataNode &node) {
     return stream << node._hash_string;
   }
@@ -108,7 +116,7 @@ public:
 
   template <class... Args>
   IDataNode(std::string class_name, Args... args)
-      : _upstream_nodes(), _downstream_nodes(), _class_name(class_name) {
+      : _upstream_nodes(), _downstream_node_hashes(), _class_name(class_name) {
     _class_scope = _scope;
     _scope == "" ? _hash_string = "" : _hash_string = _class_scope + ": ";
     _hash_string += _class_name + ": ";
@@ -118,11 +126,19 @@ public:
 
   std::vector<DataNodeShrPtr> get_upstream_nodes() { return _upstream_nodes; }
 
-  std::vector<DataNodeWeakPtr> get_downstream_nodes() { return _downstream_nodes; }
+  std::vector<DataNodeWeakPtr> get_downstream_nodes() {
+    std::vector<DataNodeWeakPtr> downstream_nodes = {};
+    for (auto &_n_hash : _downstream_node_hashes) {
+      downstream_nodes.push_back(IDataNode::__registry__[_n_hash]);
+    }
+    return downstream_nodes;
+  }
+
   std::string get_scope() { return _class_scope; }
-  void add_upstream_node(DataNodeShrPtr data) {
-    _upstream_nodes.push_back(data);
-    data->_downstream_nodes.push_back(get_weak_ptr());
+
+  void add_upstream_node(DataNodeShrPtr node) {
+    _upstream_nodes.push_back(node);
+    node->_downstream_node_hashes.push_back(_hash);
   }
 
   std::string get_hash() { return _hash; }
@@ -132,24 +148,49 @@ template <typename ClassName, typename T> class DataNode : public IDataNode {
   std::vector<T> _data;
   void _calculate() { _data_store.put_in_store(calculate()); }
 
-protected:
+public:
   template <class... Args>
   DataNode(std::string class_name = "", Args... args) : IDataNode(class_name, args...), _data(){};
+  friend class IDataNode;
+  template <typename A>
+  std::shared_ptr<DataNode<IDataNode, A>> register_output_node(std::string identifier = "") {
+    if (identifier == "") {
+      identifier = uuid::generate_uuid_v4();
+    }
+    auto output_node = std::make_shared<DataNode<IDataNode, A>>(DataNode<IDataNode, A>(identifier));
+    _output_nodes.push_back(output_node);
+    output_node->_parent_hash = _hash;
+    output_node->_parent_calc = [](std::shared_ptr<void> onode) {
+      auto parent_hash = std::static_pointer_cast<IDataNode>(onode)->_parent_hash;
+      auto parent =
+          std::static_pointer_cast<DataNode<ClassName, T>>(IDataNode::__registry__[parent_hash]);
+      parent->calculate();
+    };
+    return output_node;
+  }
 
-public:
+  static void calculate(std::shared_ptr<DataNode<ClassName, T>> node) { node->calculate(); }
+
   virtual ~DataNode() = default;
+
+  void set_data(std::vector<T> &data) {
+    _data_store.put_in_store(this, std::make_shared<std::vector<T>>(data));
+  }
+
   std::shared_ptr<std::vector<T>> get_data() {
     auto data = _data_store.retrieve(this);
     if (!data) {
-      _data_store.put_in_store(this, calculate());
+      _parent_hash != "" ? _parent_calc(shared_from_this()) : calculate();
       data = _data_store.retrieve(this);
     }
     return data;
   }
 
-  virtual std::shared_ptr<std::vector<T>> calculate() { return nullptr; }
+  virtual void calculate() {}
 
   template <class... Args> static std::shared_ptr<ClassName> create(Args... args) {
-    return std::make_shared<ClassName>(ClassName(args...));
+    auto new_node = std::make_shared<ClassName>(ClassName(args...));
+    __registry__[new_node->get_hash()] = new_node;
+    return new_node;
   }
 };
--- data_node/tests/test_example_node.cpp
+++ data_node/tests/test_example_node.cpp
@@ -9,24 +9,40 @@ class ExampleNode : public DataNode<ExampleNode, double> {
   DataNodeShrPtr _math_node;
 
 public:
-  ExampleNode(double value1 = 0, int value2 = 0)
-      : DataNode(__func__, value1, value2), _value1(value1), _value2(value2) {}
+  std::shared_ptr<DataNode<IDataNode, double>> output_1;
+  std::shared_ptr<DataNode<IDataNode, int>> output_2;
 
-  std::shared_ptr<std::vector<double>> calculate() {
-    return std::shared_ptr<std::vector<double>>(
-        new std::vector<double>({_value1, static_cast<double>(_value2)}));
+  ExampleNode(double value1 = 0, int value2 = 0)
+      : DataNode(__func__, value1, value2), _value1(value1), _value2(value2) {
+    output_1 = register_output_node<double>("output_1");
+    output_2 = register_output_node<int>();
+  }
+
+  void calculate() override {
+    auto x = std::vector<double>({_value1, static_cast<double>(_value2)});
+    set_data(x);
+    output_1->set_data(x);
+    auto y = std::vector<int>({-1, _value2});
+    output_2->set_data(y);
   };
 };
 
 class ExampleNode2 : public DataNode<ExampleNode2, int> {
   int _value5;
+  std::shared_ptr<ExampleNode> _example_node;
 
 public:
-  ExampleNode2(double value5, DataNodeShrPtr math_node = DataNode<>::create("DataNode"))
-      : DataNode<ExampleNode2, int>(__func__, value5, math_node), _value5(value5) {}
+  ExampleNode2(double value5,
+               std::shared_ptr<ExampleNode> math_node = DataNode<ExampleNode>::create())
+      : DataNode<ExampleNode2, int>(__func__, value5, math_node), _value5(value5) {
+    _example_node = math_node;
+    add_upstream_node(math_node);
+  }
 
-  std::shared_ptr<std::vector<int>> calculate() {
-    return std::shared_ptr<std::vector<int>>(new std::vector<int>({0, _value5}));
+  void calculate() override {
+    auto x = _example_node->output_2->get_data();
+    x->at(0) *= _value5;
+    set_data(*x);
   }
 };
 
@@ -34,6 +50,8 @@ TEST(DataTest, ExampleNodes) {
   auto example_one = DataNode<ExampleNode>::create(0, 1);
   auto example_two = DataNode<ExampleNode2>::create(3);
   string hash = example_one->get_hash();
-  ASSERT_EQ(example_one->get_data()->size(), 2);
   ASSERT_EQ(example_two->get_data()->size(), 2);
+  example_one->calculate();
+  auto data = example_one->output_1->get_data();
+  ASSERT_EQ(example_one->output_1->get_data()->size(), 2);
 }
