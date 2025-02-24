b43ff5117ed3870010d6e468afd06bd441f8b416
psomers3 <peter.w.somers@icloud.com>
2025-02-24 12:50:05 +0000

    add exponential moving average and fix launch.json for debugging

--- data_node_signal_processing/signal_processing.hpp
+++ data_node_signal_processing/signal_processing.hpp
@@ -3,6 +3,12 @@
 
 namespace DataNodes {
 
+/*
+A simple moving average for a single vector of values. The template value (A) needs to match the
+DataNode type that is passed to the constructor. The SimpleMovingAverage always returns a vector
+of type double.
+
+*/
 template <typename A> class SimpleMovingAverage : public DataNode<SimpleMovingAverage<A>, double> {
   std::shared_ptr<DataNode<IDataNode, A>> _input_data;
   unsigned int _window_size;
@@ -29,4 +35,29 @@ public:
     this->set_data(sma);
   }
 };
+
+template <typename A>
+class ExponentialMovingAverage : public DataNode<ExponentialMovingAverage<A>, double> {
+  std::shared_ptr<DataNode<IDataNode, A>> _input_data;
+  double _factor;
+
+public:
+  ExponentialMovingAverage(std::shared_ptr<DataNode<IDataNode, A>> node, double factor)
+      : DataNode<ExponentialMovingAverage<A>, double>(node, factor), _input_data(node),
+        _factor(factor) {}
+
+  void calculate() override {
+    std::vector<A> data = _input_data->get_data();
+    std::vector<double> ema = {};
+    ema.reserve(data.size());
+    if (data.size() >= 1) {
+      ema.push_back(data[0]);
+    }
+    for (unsigned int i = 1; i < data.size(); i++) {
+      ema.push_back((data[i] * _factor) + (ema[i - 1] * (1 - _factor)));
+    }
+    this->set_data(ema);
+  }
+};
+
 } // namespace DataNodes
