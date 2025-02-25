ec5c2f1d3fdf42ba1c18cd2492ef1dd27eb17c8a
psomers3 <peter.w.somers@icloud.com>
2025-02-25 13:07:57 +0000

    fix test

--- trader/tests/test_trader_nodes.cpp
+++ trader/tests/test_trader_nodes.cpp
@@ -14,12 +14,39 @@ TEST_F(DataNodeTest, TestCandleStickSplitter) {
     candlesticks.emplace_back(CandleStick(i, i + 1, i + 2, i + 3));
   }
 
-  auto example_one = DataNode<DataNode<IDataNode, CandleStick>, CandleStick>::create();
-  example_one->set_data(candlesticks);
-  auto high_prices = DataNode<CandleStickSplitter>::create(example_one, CandleStickPrice::HIGH);
-  auto result = high_prices->get_data();
-  EXPECT_NEAR(result.at(0), 2.0, 1e-4);
-  EXPECT_NEAR(result.at(1), 3.0, 1e-4);
-  EXPECT_NEAR(result.at(2), 4.0, 1e-4);
-  EXPECT_NEAR(result.at(3), 5.0, 1e-4);
+  auto candle_stick_node = DataNode<DataNode<IDataNode, CandleStick>, CandleStick>::create();
+  candle_stick_node->set_data(candlesticks);
+
+  auto high_prices =
+      DataNode<CandleStickSplitter>::create(candle_stick_node, CandleStickPrice::HIGH);
+  auto low_prices = DataNode<CandleStickSplitter>::create(candle_stick_node, CandleStickPrice::LOW);
+  auto open_prices =
+      DataNode<CandleStickSplitter>::create(candle_stick_node, CandleStickPrice::OPEN);
+  auto close_prices =
+      DataNode<CandleStickSplitter>::create(candle_stick_node, CandleStickPrice::CLOSE);
+
+  auto highs = high_prices->get_data();
+  auto lows = low_prices->get_data();
+  auto opens = open_prices->get_data();
+  auto closes = close_prices->get_data();
+
+  EXPECT_NEAR(highs.at(0), 2.0, 1e-4);
+  EXPECT_NEAR(highs.at(1), 3.0, 1e-4);
+  EXPECT_NEAR(highs.at(2), 4.0, 1e-4);
+  EXPECT_NEAR(highs.at(3), 5.0, 1e-4);
+
+  EXPECT_NEAR(lows.at(0), 3.0, 1e-4);
+  EXPECT_NEAR(lows.at(1), 4.0, 1e-4);
+  EXPECT_NEAR(lows.at(2), 5.0, 1e-4);
+  EXPECT_NEAR(lows.at(3), 6.0, 1e-4);
+
+  EXPECT_NEAR(opens.at(0), 0.0, 1e-4);
+  EXPECT_NEAR(opens.at(1), 1.0, 1e-4);
+  EXPECT_NEAR(opens.at(2), 2.0, 1e-4);
+  EXPECT_NEAR(opens.at(3), 3.0, 1e-4);
+
+  EXPECT_NEAR(closes.at(0), 1.0, 1e-4);
+  EXPECT_NEAR(closes.at(1), 2.0, 1e-4);
+  EXPECT_NEAR(closes.at(2), 3.0, 1e-4);
+  EXPECT_NEAR(closes.at(3), 4.0, 1e-4);
 }
