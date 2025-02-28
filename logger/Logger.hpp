6936b4c8ce6822980f2de834807a5bc6a55e3f5b
psomers3 <peter.w.somers@icloud.com>
2025-02-28 09:49:55 +0000

    change to wrapped approach for logger

--- logger/Logger.hpp
+++ logger/Logger.hpp
@@ -1,59 +1,75 @@
+#include <any>
+#include <functional>
 #include <nlohmann/json.hpp>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
-class Logger : public spdlog::logger {
-  nlohmann::json _extras;
-  std::shared_ptr<bool> _serialize;
-
-  template <typename K = std::string, typename V = std::string, typename... Args>
-  void add_to_extras(K key = "", V val = "", Args... args) {
-    if (key == "" && val == "") {
-      return;
-    }
-    this->_extras["extras"][key] = val;
-    add_to_extras(args...);
+template <typename K = std::string, typename V = std::string, typename... Args>
+void add_to_extras(nlohmann::json &extras, K key = "", V val = "", Args... args) {
+  if (key == "") {
+    return;
   }
+  extras[key] = val;
+  add_to_extras(extras, args...);
+}
+
+class Logger {
+  std::shared_ptr<bool> _serialize;
+  std::vector<nlohmann::json> _extras_patch_list;
+  std::shared_ptr<spdlog::logger> _logger;
 
   std::string adjust_message(std::string msg) {
     if (*(this->_serialize)) {
-      this->_extras["message"] = msg;
-      msg = this->_extras.dump();
+      nlohmann::json extras;
+      extras["message"] = msg;
+      for (const auto &extras_patch : this->_extras_patch_list) {
+        for (const auto &[key, val] : extras_patch.items()) {
+          extras[key] = val;
+        }
+      }
+      msg = extras.dump();
       msg.erase(0, 1);
-      this->_extras.erase("message");
     }
+    return msg;
   }
 
 public:
-  template <typename T>
-  Logger(const std::string &name, T sinks, nlohmann::json extras = {},
-         std::shared_ptr<bool> serialize = nullptr)
-      : spdlog::logger(name, sinks), _extras(extras) {
-    serialize ? _serialize = serialize : _serialize = std::make_shared<bool>(false);
+  template <typename T> Logger(const std::string &name, T sinks, nlohmann::json extras = {}) {
+    _extras_patch_list = {extras};
+    _logger = std::make_shared<spdlog::logger>(name, sinks);
+    _serialize = std::make_shared<bool>(false);
   }
 
   template <typename T>
-  Logger(const std::string &name, T sinks_start, T sinks_end, nlohmann::json extras = {},
-         std::shared_ptr<bool> serialize = nullptr)
-      : spdlog::logger(name, sinks_start, sinks_end), _extras(extras), _serialize(serialize) {
-    serialize ? _serialize = serialize : _serialize = std::make_shared<bool>(false);
+  Logger(const std::string &name, T sinks_start, T sinks_end, nlohmann::json extras = {}) {
+    _extras_patch_list = {extras};
+    _logger = std::make_shared<spdlog::logger>(name, sinks_start, sinks_end);
+    _serialize = std::make_shared<bool>(false);
   }
+
+  Logger(const Logger &logger) {
+    _logger = logger._logger;
+    _serialize = logger._serialize;
+    _extras_patch_list = logger._extras_patch_list;
+  }
+
+  std::shared_ptr<spdlog::logger> get_spdlogger() { return _logger; }
 
   void serialize(bool on = true) {
     *_serialize = on;
-    on ? this->set_pattern(
+    on ? this->_logger->set_pattern(
              "{\"timestamp\":\"%Y-%m-%d %H:%M:%S\",\"level\":\"%l\",\"logger_name\":\"%n\",%v")
-       : this->set_pattern("%+");
+       : this->_logger->set_pattern("%+");
   }
 
   template <typename... Args> std::shared_ptr<Logger> bind(Args... args) {
-    auto new_logger = std::make_shared<Logger>(this->name_, this->sinks_.begin(),
-                                               this->sinks_.end(), this->_extras, this->_serialize);
-    new_logger->add_to_extras(args...);
+    auto new_logger = std::make_shared<Logger>(*this);
+    nlohmann::json new_extras;
+    add_to_extras(new_extras, args...);
+    new_logger->_extras_patch_list.push_back(new_extras);
     return new_logger;
   }
-
-  void info(std::string msg) { return spdlog::logger::info(this->adjust_message(msg)); }
-  void debug(std::string msg) { return spdlog::logger::debug(this->adjust_message(msg)); }
-  void error(std::string msg) { return spdlog::logger::error(this->adjust_message(msg)); }
+  void info(std::string msg) { return this->_logger->info(this->adjust_message(msg)); }
+  void debug(std::string msg) { return this->_logger->debug(this->adjust_message(msg)); }
+  void error(std::string msg) { return this->_logger->error(this->adjust_message(msg)); }
 };
--- logger/tests/test_logger.cpp
+++ logger/tests/test_logger.cpp
@@ -4,17 +4,37 @@
 
 TEST(GeneralTest, TestLogger) {
   auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  auto console_sink2 = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
   auto logger = std::make_shared<Logger>("test_logger", console_sink);
-  auto new_logger = logger->bind("info", "test");
+  auto separate_logger = std::make_shared<Logger>("other_logger", console_sink2);
+  auto new_logger = logger->bind("info", "test", "number", 10);
+  auto new_logger2 = new_logger->bind("number", 30);
+  new_logger->get_spdlogger()->set_level(spdlog::level::debug);
+
   logger->info("hi");
   new_logger->info("ho");
+  separate_logger->info("hello");
+  logger->debug("hi");
+  new_logger->debug("ho");
+  separate_logger->debug("hello");
 
   logger->serialize();
+
   logger->info("hi");
   new_logger->info("ho");
+  new_logger2->info("wazzup");
+  separate_logger->info("hello");
+  logger->debug("hi");
+  new_logger->debug("ho");
+  separate_logger->debug("hello");
 
   logger->serialize(false);
+
   logger->info("hi");
   new_logger->info("ho");
+  separate_logger->info("hello");
+  logger->debug("hi");
+  new_logger->debug("ho");
+  separate_logger->debug("hello");
 }
