add65865434d212633cb1b8f5210d5256e1ddf5b
psomers3 <peter.w.somers@icloud.com>
2025-03-01 11:01:52 +0000

    add output node class to simplify syntax

--- trader/ForexInstrument.hpp
+++ trader/ForexInstrument.hpp
@@ -7,7 +7,13 @@ class ForexInstrument : public DataNode<ForexInstrument, CandleStick> {
   ForexPair _pair;
 
 public:
-  ForexInstrument(ForexPair pair) : DataNode(pair), _pair(pair) {}
+  std::shared_ptr<OutputNode<CandleStick>> candle_sticks;
+  std::shared_ptr<OutputNode<unsigned long>> utc_time_ms;
+
+  ForexInstrument(ForexPair pair) : DataNode(pair), _pair(pair) {
+    candle_sticks = register_output_node<CandleStick>();
+    utc_time_ms = register_output_node<unsigned long>();
+  }
 };
 
 } // namespace DataNodes
