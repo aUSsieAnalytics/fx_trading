91854a058485cdea9979a04ed2d8dfa524e57661
psomers3 <peter.w.somers@icloud.com>
2025-03-31 18:10:03 +0000

    run tests with http mocking exclusive to get rid of intermittent test fail

--- stonex/tests/stonex_mock.hpp
+++ stonex/tests/stonex_mock.hpp
@@ -8,30 +8,26 @@ class StoneXTest : public virtual ::testing::Test {
 public:
   static inline std::unique_ptr<httplib::Server> svr = std::make_unique<httplib::Server>();
   static inline std::thread server_thread;
-  static inline std::mutex server_mutex;
+  static inline std::unique_ptr<std::mutex> server_mutex = std::make_unique<std::mutex>();
 
   static void SetUpTestSuite() {
-    server_mutex.lock();
+    server_mutex->lock();
     setenv("STONEX_APPKEY", "TESTING", 1);
     setenv("STONEX_USER", "TESTING", 1);
     setenv("STONEX_PASS", "TESTING", 1);
 
-    svr = std::make_unique<httplib::Server>();
     svr->bind_to_port("localhost", 8080);
     std::cout << "listening on port 8080" << "\n";
     StoneX::base_url_ptr->assign("http://localhost:8080");
     StoneX::trading_url_ptr->assign("http://localhost:8080");
 
     server_thread = std::thread([]() { svr->listen_after_bind(); });
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    svr->wait_until_ready();
   }
 
   static void TearDownTestSuite() {
     svr->stop();
-    if (server_thread.joinable()) {
-      server_thread.join();
-    }
-    svr.reset();
+    server_thread.join();
     unsetenv("STONEX_APPKEY");
     unsetenv("STONEX_USER");
     unsetenv("STONEX_PASS");
@@ -40,7 +36,7 @@ public:
     StoneX::AccountCredentials::account_id = 0;
     StoneX::AccountCredentials::authorized = false;
     StoneX::AccountCredentials::initialized = false;
-    server_mutex.unlock();
+    server_mutex->unlock();
   }
   void SetUp() override {
     StoneX::AccountCredentials::authorized = false;
